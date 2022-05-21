a8da4a4bafcb03fc3444e1a263e0adec2db9f59b
yerim08 <68000705+yerim08@users.noreply.github.com>
2022-05-21 16:26:35 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf8').split(' ');
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A+B);
+console.log(A-B);
+console.log(A*B);
+console.log(parseInt(A/B));
+console.log(A%B);
